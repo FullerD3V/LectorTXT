211099417a036f13a31007b59c8cb69cff334265
fullerd3v <fullerd3v@gmail.com>
2022-10-11 19:46:30 +0200

    apertura de ejercicios

--- src/lectortxt/Main/FXMLDocumentController.java
+++ src/lectortxt/Main/FXMLDocumentController.java
@@ -5,13 +5,18 @@
  */
 package lectortxt.Main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -20,19 +25,47 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Button btn1, btn2, btn3, btn4;
+    public Button btn1, btn2, btn3, btn4;
     
     @FXML
-    public void btn1OnAction(){}
+    public void btn1OnAction(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(lectortxt.LectorTXT.class.getResource("Ex1/FXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
     
     @FXML
-    public void btn2OnAction(){}
+    public void btn2OnAction() throws IOException{
+        Parent root = FXMLLoader.load(lectortxt.LectorTXT.class.getResource("Ex2/FXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
     
     @FXML
-    public void btn3OnAction(){}
+    public void btn3OnAction() throws IOException{
+        Parent root = FXMLLoader.load(lectortxt.LectorTXT.class.getResource("Ex3/FXML.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
     
     @FXML
-    public void btn4OnAction(){}
+    public void btn4OnAction() throws IOException{
+        Parent root = FXMLLoader.load(lectortxt.LectorTXT.class.getResource("Ex4/FXML.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
