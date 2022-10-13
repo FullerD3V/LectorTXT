983aa4a25d6011998c0633799723ac56663ed2e8
fullerd3v <fullerd3v@gmail.com>
2022-10-13 16:42:18 +0200

    Ejercicio 2 funcional

--- src/lectortxt/Ex2/FXMLController.java
+++ src/lectortxt/Ex2/FXMLController.java
@@ -5,9 +5,17 @@
  */
 package lectortxt.Ex2;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +23,20 @@ import javafx.fxml.Initializable;
  * @author alumno
  */
 public class FXMLController implements Initializable {
+    
+    @FXML
+    private Button btnGuardar;
+    
+    @FXML
+    private TextField txt;
+    
+    @FXML
+    public void btnGuardarOnAction(ActionEvent event) throws IOException{
+        FileWriter myWriter = new FileWriter("/home/alumno/NetBeansProjects/LectorTXT/src/lectortxt/Ex2/Hola.txt", true);
+        myWriter.write(txt.getText() + "\n" );
+        myWriter.close();    
+    }
+        
 
     /**
      * Initializes the controller class.
